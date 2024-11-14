5929237a09091e9b45e493836617086cbcb1a495
iKrazyCode <dodochavescadete13@gmail.com>
2024-11-14 09:30:34 -0300

    update

--- custom_admin/utils.py
+++ custom_admin/utils.py
@@ -10,31 +10,49 @@ def form_injection(text_html, template_copy):
 
     action = "{% url 'home:renderizator' url=URL %}"
 
+
+    # Remove os action e method dos forms, para em seguida adicionar novos action e method ao form
     texto = re.sub(
         r'(action[ ]*=[ ]*[\'"])(.*?)([\'"][ ]*)',
-        r'\1{}\3'.format(action),
+        r'',
         texto,
         flags=re.IGNORECASE | re.DOTALL
     )
 
     texto = re.sub(
         r'(method[ ]*=[ ]*[\'"])(.*?)([\'"][ ]*)',
-        r'\1{}\3'.format('POST'),
+        r'',
         texto,
         flags=re.IGNORECASE | re.DOTALL
     )
 
     texto = re.sub(
-        r'(<form.*?>)',
-        r'\1{% csrf_token %}',
+        r'(<form)(.*?)(>)',
+        r'\1 action="{}" method="POST" \2\3{}'.format(action, "{% csrf_token %}"),
         texto,
         flags=re.IGNORECASE | re.DOTALL
     )
 
     # script: remove todas tags script
-    texto5555 = re.sub(
+    texto = re.sub(
         r'(<script.*?>)(.*?)(</script>)',
         r'',
+        texto,
+        flags=re.IGNORECASE | re.DOTALL
+    )
+
+    # base: remove todas tags base
+    texto = re.sub(
+        r'(<base.*?>)(.*?)',
+        r'',
+        texto,
+        flags=re.IGNORECASE | re.DOTALL
+    )
+
+    # remover ../../: remove todas tags script
+    texto = re.sub(
+        r'(\.\./)+',
+        r'/',
         texto,
         flags=re.IGNORECASE | re.DOTALL
     )
@@ -53,6 +71,22 @@ def form_injection(text_html, template_copy):
     # href: adicionar o dominio do site no começo 
     texto = re.sub(
         r'(href[ ]*=[ ]*[\'"]?)\.?\/(.*?)([\'"][ ]*?)',
+        r'\1{}\2\3'.format(template_copy_url_trated),
+        texto,
+        flags=re.IGNORECASE | re.DOTALL
+    )
+
+    # Para src que não contem / no inicio
+    texto = re.sub(
+        r'(src\s*=\s*[\'"])(?!https?://)(/?[^\'"]+)([\'"])',
+        r'\1{}\2\3'.format(template_copy_url_trated),
+        texto,
+        flags=re.IGNORECASE | re.DOTALL
+    )
+
+    # para href que não contem / no inicio 
+    texto = re.sub(
+        r'(href\s*=\s*[\'"])(?!https?://)(/?[^\'"]+)([\'"])',
         r'\1{}\2\3'.format(template_copy_url_trated),
         texto,
         flags=re.IGNORECASE | re.DOTALL
