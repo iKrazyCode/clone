f7405dcfada7543b43fc21a85d117ffe4a23d8a7
iKrazyCode <dodochavescadete13@gmail.com>
2024-11-20 12:50:53 -0300

    client validations

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gg06y(883z&+!6sk3ub@bg2r8o3k=ya-vkpm0v@#o+o33bya9*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
